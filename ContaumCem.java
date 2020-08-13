7e17e8ef1e09a0b8b4be43a3939806f5d824723e
Cassio Silva <cassiopereira32@hotmail.com>
2020-08-13 14:48:19 -0300

    Contador de 1 a 10 com WHILE em java

---
+++ ContaumCem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContaumCem{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int cont = 1;
+        while (cont <= 10) {
+            System.out.println("Contador de 1 a 10 = " + cont++);
+
+        }
+
+
+    }
+
+}
